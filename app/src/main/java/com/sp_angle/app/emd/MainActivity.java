addbbf539e28490428dce382a75b2436b64914ef
sp_angle <kirufin3752@gmail.com>
2017-04-13 23:03:19 +0900

    2017/04/13 23:03

---
+++ app/src/main/java/com/sp_angle/app/emd/MainActivity.java
@@ -0,0 +1,12 @@
+package com.sp_angle.app.emd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
